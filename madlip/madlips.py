aeb9c1ac66bad1934576147cee2e094e18c0ab31
2D--Game <karpuzcagri@gmail.com>
2023-08-03 21:06:48 +0300

    Create madlips.py

---
+++ madlip/madlips.py
@@ -0,0 +1,9 @@
+adj = input("Adjectives: ")
+verb = input("Verb :")
+anime = input("Best anime for you: ")
+adj2 = input("Adjectives: ")
+
+madlip = f'Anime is {adj}!!! I love to watch them because its make me {verb}. \
+      I really pround myseşf tp watch {anime}. It was {adj2}'
+
+print(madlip)
